3f105b691cabef0f08ee882648dec50c07be2e70
yuemenglong <yuemenglong@126.com>
2017-12-05 17:29:13 +0800

    add：课程体系

--- src/main/scala/com/cchtrip/stop/controller/CourseCategoryCtr.scala
+++ src/main/scala/com/cchtrip/stop/controller/CourseCategoryCtr.scala
@@ -6,6 +6,7 @@ import com.cchtrip.stop.util.NamedException
 import io.github.yuemenglong.json.JSON
 import io.github.yuemenglong.orm.Orm
 import io.github.yuemenglong.orm.lang.types.Types._
+import io.github.yuemenglong.orm.tool.OrmTool
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation._
@@ -43,7 +44,40 @@ class CourseCategoryCtr {
   })
 
   @GetMapping(Array(""))
-  def getCategory: String = {
-    throw new NamedException("Name", "Test")
-  }
+  def getCategoryAll: String = dao.beginTransaction(session => {
+    val root = Orm.root(classOf[CourseCategory])
+    val res = session.query(Orm.selectFrom(root))
+    val map: Map[Long, CourseCategory] = res.map(c => (c.id, c))(collection.breakOut)
+    res.foreach(c => {
+      c.children = Array()
+      if (map.contains(c.parentId)) {
+        map(c.parentId).children ++= Array(c)
+      }
+    })
+    val ret = map.values.filter(_.level == 0).toArray
+    JSON.stringify(ret)
+  })
+
+  @DeleteMapping(Array("/{id}"))
+  def deleteCategory(@PathVariable id: Long): String = dao.beginTransaction(session => {
+    //1. 没有子节点才能删
+    //2. 没有课程相关联才能删
+    {
+      val root = Orm.root(classOf[CourseCategory])
+      val count = session.first(Orm.select(root.count()).from(root).where(root.get("parentId").eql(id)))
+      if (count > 0) {
+        throw NamedException("DEL_CATE_FAIL", "子类型不为空")
+      }
+    }
+    {
+      val root = Orm.root(classOf[Course])
+      val count = session.first(Orm.select(root.count()).from(root).where(root.get("categoryId").eql(id)))
+      if (count > 0) {
+        throw NamedException("DEL_CATE_FAIL", "还有课程相关联")
+      }
+    }
+    OrmTool.deleteById(classOf[CourseCategory], id, session)
+    "{}"
+  })
+
 }
--- src/main/scala/com/cchtrip/stop/util/NamedException.scala
+++ src/main/scala/com/cchtrip/stop/util/NamedException.scala
@@ -3,6 +3,6 @@ package com.cchtrip.stop.util
 /**
   * Created by <yuemenglong@126.com> on 2017/12/5.
   */
-class NamedException(val name: String, message: String) extends RuntimeException(message) {
+case class NamedException(name: String, message: String) extends RuntimeException(message) {
 
 }

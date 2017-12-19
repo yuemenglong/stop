e0d7bcc725cd0bd176400cd93cac17e687c48a1a
yuemenglong <yuemenglong@126.com>
2017-12-19 18:34:02 +0800

    add: 题目管理

---
+++ src/main/scala/com/cchtrip/stop/controller/admin/CoursewareCtr.scala
@@ -0,0 +1,71 @@
+package com.cchtrip.stop.controller.admin
+
+import com.cchtrip.stop.bean.Dao
+import com.cchtrip.stop.entity._
+import com.cchtrip.stop.util.NamedException
+import io.github.yuemenglong.json.JSON
+import io.github.yuemenglong.orm.Orm
+import io.github.yuemenglong.orm.lang.types.Types._
+import io.github.yuemenglong.orm.tool.OrmTool
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation._
+
+/**
+  * Created by <yuemenglong@126.com> on 2017/11/21.
+  */
+@Controller
+@RequestMapping(value = Array("/admin/courseware"), produces = Array("application/json"))
+@ResponseBody
+class CoursewareCtr {
+
+  @Autowired
+  var dao: Dao = _
+
+  @PostMapping(Array(""))
+  def post(@RequestBody body: String): String = dao.beginTransaction(session => {
+    val obj = JSON.parse(body, classOf[Courseware])
+    obj.crTime = new Date
+    session.execute(Orm.insert(obj))
+    JSON.stringify(obj)
+  })
+
+  @PutMapping(Array("/{id}"))
+  def put(@PathVariable id: Long, @RequestBody body: String): String = dao.beginTransaction(session => {
+    val obj = JSON.parse(body, classOf[Courseware])
+    obj.id = id
+    session.execute(Orm.update(obj))
+    JSON.stringify(obj)
+  })
+
+  @GetMapping(Array("/list"))
+  def list(@RequestParam(defaultValue = "20") limit: Long,
+           @RequestParam(defaultValue = "0") offset: Long
+          ): String = dao.beginTransaction(session => {
+    val root = Orm.root(classOf[Courseware])
+    val query = Orm.selectFrom(root).limit(limit).offset(offset)
+    val res = session.query(query)
+    JSON.stringify(res)
+  })
+
+  @GetMapping(Array("/count"))
+  def count(): String = dao.beginTransaction(session => {
+    val root = Orm.root(classOf[Student])
+    val query = Orm.select(root.count()).from(root)
+    val res = session.first(query)
+    JSON.stringify(res)
+  })
+
+  @GetMapping(Array("/{id}"))
+  def get(@PathVariable id: Long): String = dao.beginTransaction(session => {
+    val res = OrmTool.selectById(classOf[Courseware], id, session)
+    JSON.stringify(res)
+  })
+
+  @DeleteMapping(Array("/{id}"))
+  def delete(@PathVariable id: Long): String = dao.beginTransaction(fn = session => {
+    OrmTool.deleteById(classOf[Courseware], id, session)
+    "{}"
+  })
+
+}
---
+++ src/main/scala/com/cchtrip/stop/controller/admin/QuestionCtr.scala
@@ -0,0 +1,80 @@
+package com.cchtrip.stop.controller.admin
+
+import com.cchtrip.stop.bean.Dao
+import com.cchtrip.stop.entity._
+import io.github.yuemenglong.json.JSON
+import io.github.yuemenglong.orm.Orm
+import io.github.yuemenglong.orm.lang.types.Types._
+import io.github.yuemenglong.orm.operate.traits.core.Root
+import io.github.yuemenglong.orm.tool.OrmTool
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation._
+
+/**
+  * Created by <yuemenglong@126.com> on 2017/11/21.
+  */
+@Controller
+@RequestMapping(value = Array("/admin/question"), produces = Array("application/json"))
+@ResponseBody
+class QuestionCtr {
+
+  @Autowired
+  var dao: Dao = _
+
+  @PostMapping(Array(""))
+  def post(@RequestBody body: String): String = dao.beginTransaction(session => {
+    val obj = JSON.parse(body, classOf[Question])
+    obj.crTime = new Date
+    val ex = Orm.insert(obj)
+    ex.insert("sc")
+    session.execute(ex)
+    JSON.stringify(obj)
+  })
+
+  @PutMapping(Array("/{id}"))
+  def put(@PathVariable id: Long, @RequestBody body: String): String = dao.beginTransaction(session => {
+    val obj = JSON.parse(body, classOf[Question])
+    obj.id = id
+    val ex = Orm.update(obj)
+    ex.update("sc")
+    session.execute(ex)
+    JSON.stringify(obj)
+  })
+
+  @GetMapping(Array("/list"))
+  def list(@RequestParam(defaultValue = "20") limit: Long,
+           @RequestParam(defaultValue = "0") offset: Long
+          ): String = dao.beginTransaction(session => {
+    val root = Orm.root(classOf[Question])
+    root.select("sc")
+    val query = Orm.selectFrom(root).limit(limit).offset(offset)
+    val res = session.query(query)
+    JSON.stringify(res)
+  })
+
+  @GetMapping(Array("/count"))
+  def count(): String = dao.beginTransaction(session => {
+    val root = Orm.root(classOf[Student])
+    val query = Orm.select(root.count()).from(root)
+    val res = session.first(query)
+    JSON.stringify(res)
+  })
+
+  @GetMapping(Array("/{id}"))
+  def get(@PathVariable id: Long): String = dao.beginTransaction(session => {
+    val res = OrmTool.selectById(classOf[Question], id, session, (root: Root[Question]) => {
+      root.select("sc")
+    })
+    JSON.stringify(res)
+  })
+
+  @DeleteMapping(Array("/{id}"))
+  def delete(@PathVariable id: Long): String = dao.beginTransaction(fn = session => {
+    OrmTool.deleteById(classOf[Question], id, session, (root: Root[Question]) => {
+      Array(root.leftJoin("sc"))
+    })
+    "{}"
+  })
+
+}
